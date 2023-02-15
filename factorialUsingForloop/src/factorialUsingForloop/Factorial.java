e05f2e136a33dfb751246326fa31529d89e771a0
shashank <shashank944533@gmail.com>
2023-02-15 20:35:58 +0530

    Factorial of a number

---
+++ factorialUsingForloop/src/factorialUsingForloop/Factorial.java
@@ -0,0 +1,30 @@
+package factorialUsingForloop;
+
+import java.util.Scanner;
+
+public class Factorial {
+public static void main(String[] args) {
+	int number;
+	long factorial = 1;
+	Scanner input  =  new Scanner(System.in);
+	System.out.println("Enter the number");
+	number = input.nextInt();
+	input.close();
+	if(number<0)
+	{
+		System.out.println("Can't find the factorial of a negative number");
+	}
+	else if(number<=1)
+	{
+		System.out.printf("%d!=%d",number,factorial);
+	}
+	else
+	{
+		for(int counter=number;counter>=2;counter--)
+		{
+			factorial = factorial * counter;
+		}
+		System.out.printf("%d!=%d",number,factorial);
+	}
+}
+}
